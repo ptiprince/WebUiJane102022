8f824de92aafce592be806bd52771ce2df550a81
ptiprince <88402623+ptiprince@users.noreply.github.com>
2022-10-14 16:04:19 -0400

    Initial commit

---
+++ src/main/java/ru/gb/App.java
@@ -0,0 +1,15 @@
+package ru.gb;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
